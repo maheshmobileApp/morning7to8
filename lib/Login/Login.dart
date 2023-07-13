3098c85a3e386b4a3b83a3f2212d7a2fe1e4f085
mahesh <yernagulamahesh@gmail.com>
2023-07-13 23:42:18 +0530

    login validation

--- lib/Login/Login.dart
+++ lib/Login/Login.dart
@@ -8,6 +8,17 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController _emailControll = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+//step 1: create form key
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  //step 2: wrap the widgets into Form
+
+  //step 3: assign the formkey to Form widget
+  //step 4: add the validator to textfield with some condition
+  //step 5: on the button action call the all validators
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,25 +40,118 @@ class _LoginState extends State<Login> {
           ],
           centerTitle: true,
         ),
-        backgroundColor: Color.fromRGBO(128, 0, 255, 1),
-        body: Column(
-          children: [
-            createContainer(Colors.red,Colors.black45,1),
-            createContainer(Colors.blue,Colors.black45,2),
-            createContainer(Colors.orange,Colors.black45,3)
-          ],
+        backgroundColor: Colors.white,
+        body: Container(
+          color: Colors.white,
+          margin: EdgeInsets.symmetric(horizontal: 25),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _emailControll,
+                  keyboardType: TextInputType.emailAddress,
+                  // maxLines: 1,
+                  decoration: InputDecoration(
+                      hintText: "Enter Email",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please Enter Email";
+                    } else if (!isValidEmail(value)) {
+                      return "Please enter Valid email";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                    obscureText: true,
+                    controller: _passwordController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        
+                        return "Please Enter Password";
+                      
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                        helperText:
+                            "password should be upper,lower and special char",
+                        hintText: "Enter Password",
+                        labelText: "Enter password",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)))),
+                SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                    onPressed: () {
+                      print("email value is ${_emailControll.text}");
+                      print("password value is ${_passwordController.text}");
+                      if (formKey.currentState!.validate()) {
+                        print("form is validated");
+                      } else {
+                        print("Not valid");
+                      }
+                    },
+                    child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 5),
+                          child: Text("Login"),
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Colors.black, width: 1))))
+              ],
+            ),
+          ),
         ));
   }
 
-  Widget createContainer(Color color,Color borderColor, int number) {
+  bool isValidEmail(String data) {
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegExp.hasMatch(data);
+  } /*
+  
+  Container(
+          width: 400,
+          height: 400,
+          color: Colors.red,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                createContainer(Colors.red,Colors.black45,1),
+                createContainer(Colors.blue,Colors.black45,2),
+                createContainer(Colors.orange,Colors.black45,3),
+                  createContainer(Colors.orange,Colors.black45,3)
+              ],
+            ),
+          ),
+        )
+   */
+
+  Widget createContainer(Color color, Color borderColor, int number) {
     return Container(
       margin: EdgeInsets.all(20),
       decoration: BoxDecoration(
           color: color,
           borderRadius: BorderRadius.circular(20),
-          border: Border.all(color:borderColor, width: 5)),
-      width: 200,
-      height: 200,
+          border: Border.all(color: borderColor, width: 5)),
+      width: 100,
+      height: 100,
       // color: Color.fromRGBO(204, 204, 255, 1),
       padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
       child: Container(
@@ -65,3 +169,4 @@ class _LoginState extends State<Login> {
 }
 
 //opacity range between 0 to 1
+// we have two textfields
